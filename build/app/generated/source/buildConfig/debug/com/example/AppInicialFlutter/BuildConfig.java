fef7401a2125dbd15c7155b01ad2c2b71b3bf7af
javiervinus <javiervin@hotmail.com>
2020-09-06 22:57:02 -0500

    actualizado

---
+++ build/app/generated/source/buildConfig/debug/com/example/AppInicialFlutter/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.AppInicialFlutter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.AppInicialFlutter";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
